7e28470c8eb350af38ab5da538770f547a195a8c
keshav-star <keshavsandhu00@gmail.com>
2024-01-07 12:46:14 +0530

    build deploy

--- server/server.js
+++ server/server.js
@@ -10,18 +10,18 @@ const botRoute = require('./botScript')
 const app = express()
 const port = process.env.PORT || 4000
 app.use(cors());
-// const buildPath = path.join(__dirname, '../build')
+const buildPath = path.join(__dirname, '../build')
 
 connectionDB()
-// app.use(express.static(buildPath))
+app.use(express.static(buildPath))
 app.use(express.json())
 
 app.use('/bot',botRoute)
 
-// gets the static files from the build folder
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(buildPath, '/index.html'))
-// })
+gets the static files from the build folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, '/index.html'))
+})
 
 
 
